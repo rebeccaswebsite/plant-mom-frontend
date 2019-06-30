e126c8050cf81d6124e2a48aacde97a824dae456
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-06-30 22:52:22 +0100

    add room component

---
+++ src/components/Room.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PlantCard from 'PlantCard'
+
+class Room extends React.Component {
+
+  render () {
+    const { room } = this.props
+
+    return (
+      <div>
+          <h1>{room.name}</h1>
+      </div>
+    )
+  }
+}
+
+export default Room
